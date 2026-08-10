f002ad6ebd6e944cf3ef317767bdbf39c45ac608
Hitesh Tech <hiteshpant50@gmail.com>
2026-08-10 22:11:12 +0545

    Delete backend directory

--- backend/routes/webhookRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const webhookController = require('../controllers/webhookController');
-
-// Stripe requires the raw body to construct the event
-router.post('/stripe', express.raw({ type: 'application/json' }), webhookController.stripeWebhook);
-
-module.exports = router;
